e31dd1d3cb4f0dc0dc4ac5201b57dac4ca453c60
Yevhen <evgen41293@gmail.com>
2019-12-13 16:58:49 +0200

    feat: improved error messages

--- src/util/http.js
+++ src/util/http.js
@@ -14,9 +14,17 @@ function createClient() {
 
 function handleError(error) {
     if (typeof error === 'string' || error instanceof String) {
-        ui.message(error);
+        return ui.message(error);
     } else {
-        ui.message(`An error occurred ${JSON.stringify(error)}`);
+        if (error.error) {
+            const httpError = error.error;
+            if (httpError.code && httpError.code === 403) {
+                return ui.message("You don't have enough permissions to perform this action");
+            } else if (httpError.message) {
+                return ui.message(httpError.message);
+            }
+        }
+        return ui.message(`An error occurred ${JSON.stringify(error)}`);
     }
 }
 
