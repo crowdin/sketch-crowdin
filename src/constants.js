f2d683d15eb687bc4887b99bb8f3c65257504196
Yevhen <evgen41293@gmail.com>
2020-02-12 21:43:10 +0200

    feat: added sketch plugin user agent

--- src/constants.js
+++ src/constants.js
@@ -3,3 +3,4 @@ export const ORGANIZATION = 'crowdin-organization';
 export const PROJECT_ID = 'crowdin-project-id';
 export const SYMBOL_TYPE = 'symbol-override';
 export const TEXT_TYPE = 'text';
+export const PLUGIN_VERSION = '1.2.0';
