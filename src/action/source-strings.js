02923f5897f3997cb07fc06e480a9b09053c725d
Yevheniy <yevheniy@Victors-MacBook-Air.local>
2020-11-01 14:13:55 +0200

    fixed issue with editing

--- src/action/source-strings.js
+++ src/action/source-strings.js
@@ -55,6 +55,7 @@ async function useString(strings) {
             const text = string.text;
 
             if (selectedText.type === TEXT_TYPE) {
+                selectedText.element.sketchObject.finishEditing();
                 selectedText.element.text = text;
                 selectedText.element.name = !!string.identifier ? string.identifier : text;
             } else {
