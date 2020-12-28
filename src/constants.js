c43b4cb92bec57224b64e9a2ea37d95db0a6f962
Andrii Bodnar <andrii.bodnar@crowdin.com>
2020-12-28 11:30:22 +0200

    Version 2.2.0

--- src/constants.js
+++ src/constants.js
@@ -10,7 +10,7 @@ export const KEY_NAMING_PATTERN = `${KEY_PREFIX}-key-naming`;
 export const SYMBOL_TYPE = 'symbol-override';
 export const TEXT_TYPE = 'text';
 
-export const PLUGIN_VERSION = '2.1.2';
+export const PLUGIN_VERSION = '2.2.0';
 
 export const STRINGS_KEY_NAMING_OPTIONS = [
     { id: 1, name: 'Artboard.Group.Element_name', },
