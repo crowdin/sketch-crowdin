6d7b412c047104f652ece1f166c8eb138a40cc09
Yevheniy <yevheniy@000003.local>
2021-08-31 13:52:22 +0300

    reverted back flag for development

--- src/plugin.js
+++ src/plugin.js
@@ -29,7 +29,7 @@ const identifier = 'crowdin';
 export default function start() {
 
     //set to true for local development
-    const devTools = true;
+    const devTools = false;
 
     const options = {
         identifier,
